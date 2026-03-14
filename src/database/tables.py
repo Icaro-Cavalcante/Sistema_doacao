f1308d683e7530750ff31fa2754338f1769f5e2c
Icaro-Cavalcante <icarodev06@gmail.com>
2026-03-14 15:14:34 -0300

    Tabelas atualizadas
    - `Tabelas atualizadas para se equiparar ao modelo conceitual`
    
    Co-Authored-By: Aisha <aisha.tomaz@aluno.ufca.edu.br>

--- src/database/tables.py
+++ src/database/tables.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Table, String, Column, MetaData, Integer, Date, ForeignKey, Boolean
+from datetime import date
 from src.database.database import Database
 database = Database()
 
@@ -13,20 +14,18 @@ class Tabela():
 # <--------------------------------------------------Tabelas-------------------------------------------------->
              
             # Doações
-        self.doacao = Table(
-            'doacao', self.metadata,
-            Column('id_doacao', Integer, primary_key=True, autoincrement=True),
-            Column('id_usuario', Integer, ForeignKey('usuario.id_usuario'), nullable=False),
+        self.doacoes = Table('doacoes', self.metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
             Column('data_doacao', Date, nullable=False),
             Column('motivo_recusa', String(255)),
             Column('status_atual', String(40), nullable=False)
         )
 
-        self.doacaoItem = Table(
-            'doacaoItem', self.metadata,
+        self.doacoesItem = Table('doacoesItem', self.metadata,
             Column('id_doacao', Integer, ForeignKey('doacao.id_doacao'), primary_key=True),
             Column('id_item', Integer, ForeignKey('itens.id_item'), primary_key=True),
-            Column('quantidade', Integer, nullable=False)
+            Column('quantidade_utilizada', Integer, nullable=False)
         )
 
             # Usuários
@@ -41,54 +40,61 @@ class Tabela():
             Column('tipo_perfil', String(40))
         )
 
-        self.pessoaJuridica = Table('pessoaJuridica', self.metadata,
-            Column('id_usuario', Integer, ForeignKey('usuario.id_usuario'), primary_key=True),
+        self.pessoasJuridica = Table('pessoasJuridica', self.metadata,
+            Column('id_usuario', Integer, ForeignKey('usuario.id'), primary_key=True),
             Column('user_cnpj', String(14), unique=True, nullable=False),
             Column('razao_social', String(100), nullable=False)
         )
 
-        self.pessoaFisica = Table('pessoaFisica', self.metadata,
-            Column('id_usuario', Integer, ForeignKey('usuario.id_usuario'), primary_key=True),
+        self.pessoasFisica = Table('pessoaFisica', self.metadata,
+            Column('id_usuario', Integer, ForeignKey('usuarios.id'), primary_key=True),
             Column('user_cpf', String(11), unique=True, nullable=False),
             Column('data_nascimento', Date)
         )
 
             # Categoria
-        self.itemCategoria = Table('itemCategoria', self.metadata,
-            Column('id_categoria', Integer, primary_key=True, autoincrement=True),
+        self.itensCategoria = Table('itemCategoria', self.metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
             Column('nome_categoria', String(50), unique=True, nullable=False)
         )
 
             # Itens
         self.itens = Table('itens', self.metadata,
-            Column('id_item', Integer, primary_key=True, autoincrement=True),
-            Column('id_categoria', Integer, ForeignKey('itemCategoria.id_categoria')),
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('id_categoria', Integer, ForeignKey('itemCategoria.id')),
             Column('descricao', String(255)),
             Column('unidade_medida', String(50))
         )
 
             # Distribuição
-        self.distribuicao = Table('distribuicao', self.metadata,
-            Column('id_distribuicao', Integer, primary_key=True, autoincrement=True),
-            Column('id_pedido', Integer, ForeignKey('pedidoAuxilio.id_pedido')),
-            Column('id_doacao', Integer, ForeignKey('doacao.id_doacao')),
+        self.distribuicoes = Table('distribuicao', self.metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('id_pedido', Integer, ForeignKey('pedidoAuxilio.id')),
+            Column('id_doacao', Integer, ForeignKey('doacoes.id')),
             Column('user_cnpj', String(14), ForeignKey('pessoaJuridica.user_cnpj')),
             Column('data_entrega', Date, nullable=False),
             Column('validacao_recebimento', Boolean, default=False)
         )
         
-        self.distribuicaoItem = Table('distribuicaoItem', self.metadata,
-            Column('id_distribuicao', Integer, ForeignKey('distribuicao.id_distribuicao'), primary_key=True),
-            Column('id_item', Integer, ForeignKey('itens.id_item'), primary_key=True),
-            Column('user_cnpj', String(14), ForeignKey('pessoaJuridica.user_cnpj'))
+        self.distribuicoesItem = Table('distribuicoesItem', self.metadata,
+            Column('id', Integer, ForeignKey('distribuicoes.id'), primary_key=True),
+            Column('id_item', Integer, ForeignKey('itens.id'), primary_key=True),
+            Column('user_cnpj', String(14), ForeignKey('pessoaJuridica.user_cnpj')),
+            Column('quantidade_utlizada', Integer, nullable=False)
         )
 
-        # TODO: self.rastreio = Table('rastreio', self.metadata,) 
+        self.rastreios = Table('rastreios', self.metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('id_doacao', Integer, ForeignKey('doacoes.id'), nullable=False),
+            Column('data_hora', date, nullable=False),
+            Column('etapa', String(100)),
+            Column('localizacao', String(100))
+        )   
            
             # Pedido de auxílio
-        self.pedidoAuxilio = Table('pedidoAuxilio', self.metadata,
-            Column('id_pedido', Integer, primary_key=True, autoincrement=True),
-            Column('id_usuario', Integer, ForeignKey('usuario.id_usuario'), nullable=False),
+        self.pedidosAuxilio = Table('pedidosAuxilio', self.metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
             Column('flag_fraude', Boolean, default=False),
             Column('status', String(40), nullable=False),
             Column('justificativa', String(500), nullable=False),
@@ -96,19 +102,19 @@ class Tabela():
         )
 
             # Voluntariado
-        self.vagaVoluntario = Table('vagaVoluntario', self.metadata,
-            Column('id_vaga', Integer, primary_key=True, autoincrement=True),
-            Column('id_usuario', Integer, ForeignKey('usuario.id_usuario'), nullable=False),
+        self.vagasVoluntario = Table('vagasVoluntario', self.metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
             Column('data_evento', Date, nullable=False),
             Column('carga_horaria', String(50)),
             Column('titulo', String(100), nullable=False),
             Column('descricao', String(500), nullable=False)
         )
         
-        self.inscricao = Table('inscricao', self.metadata,
-            Column('id_inscricao', Integer, primary_key=True, autoincrement=True),
-            Column('id_vaga', Integer, ForeignKey('vagaVoluntario.id_vaga'), nullable=False),
-            Column('id_usuario', Integer, ForeignKey('usuario.id_usuario'), nullable=False),
+        self.inscricoes = Table('inscricoes', self.metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('id_vaga', Integer, ForeignKey('vagasVoluntario.id'), nullable=False),
+            Column('id_usuario', Integer, ForeignKey('usuarios.id'), nullable=False),
             Column('data_inscricao', Date, nullable=False),
             Column('status', String(40), nullable=False),
             Column('checkin_presenca', Boolean, default=False)
